8879aa2a98fa9d2b4f6eebeb98415b148f8981ee
Jung Soo Ha <jungsooha@Jungui-MacBookPro.local>
2021-06-01 22:34:59 +0900

    seed fake data

---
+++ reviews/management/commands/seed_reviews.py
@@ -0,0 +1,39 @@
+import random
+from django.core.management.base import BaseCommand
+from django_seed import Seed
+from reviews import models as review_models
+from users import models as user_models
+from rooms import models as room_models
+
+
+class Command(BaseCommand):
+    help = "Seed reviews"
+
+    def add_arguments(self, parser):
+        parser.add_argument(
+            "--number", default=1, type=int, help="number of reviews to be created"
+        )
+        return super().add_arguments(parser)
+
+    def handle(self, *args, **options):
+        number = options.get("number")
+        seeder = Seed.seeder()
+        users = user_models.User.objects.all()
+        rooms = room_models.Room.objects.all()
+        seeder.add_entity(
+            review_models.Review,
+            number,
+            {
+                "accuracy": lambda x: random.randint(0, 5),
+                "communication": lambda x: random.randint(0, 5),
+                "cleanliness": lambda x: random.randint(0, 5),
+                "location": lambda x: random.randint(0, 5),
+                "check_in": lambda x: random.randint(0, 5),
+                "value": lambda x: random.randint(0, 5),
+                "user": lambda x: random.choice(users),
+                "room": lambda x: random.choice(rooms),
+            },
+        )
+
+        seeder.execute()
+        self.stdout.write(self.style.SUCCESS(f"{number} reviews created"))
