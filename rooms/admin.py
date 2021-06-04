34b9877cb0f5ca6e6a93c0071e70d4cd7386f12c
Jung Soo Ha <jungsooha@Jungui-MacBookPro.local>
2021-06-04 15:45:13 +0900

    finished search view

--- rooms/admin.py
+++ rooms/admin.py
@@ -40,7 +40,17 @@ class RoomAdmin(admin.ModelAdmin):
     fieldsets = (
         (
             "Basic Info",
-            {"fields": ("name", "description", "country", "city", "address", "price")},
+            {
+                "fields": (
+                    "name",
+                    "description",
+                    "country",
+                    "city",
+                    "address",
+                    "room_type",
+                    "price",
+                )
+            },
         ),
         ("Times", {"fields": ("check_in", "check_out", "instant_book")}),
         ("Spaces", {"fields": ("guests", "beds", "bedrooms", "baths")}),
